1bc1085fc16b27a0230b1902b160d00a52b7b7f3
“Your <you@example.com>
2024-01-04 21:12:23 +0100

    search.test

--- __tests__/api/getCountryByName.test.js
+++ __tests__/api/getCountryByName.test.js
@@ -1,6 +1,21 @@
+import axios from "axios";
+import {getCountryByName} from "../../src/api/getCountryByName";
 //TODO: write a mock to simulate data being fetched from the API
 //Use this to help: https://dev.to/zaklaughton/the-only-3-steps-you-need-to-mock-an-api-call-in-jest-39mb
+jest.mock('axios')
+test('Get country by Name return countries', async () => {
+    axios.get.mockResolvedValue({
+        data: [
+            {
+                name: {
+                    common: 'spain',
 
-test('REPLACE_ME', async () => {
-    expect("REPLACE_ME").toBe("REPLACE_ME");
+                }
+            },
+        ]
+    })
+
+    const countries = await getCountryByName('spain');
+    expect(countries.length).toEqual(1);
+    expect(countries[0].name.common).toEqual('spain');
 });
--- __tests__/components/Search.test.js
+++ __tests__/components/Search.test.js
@@ -1,7 +1,8 @@
-import renderer from 'react-test-renderer'; //you will need this for the snapshot test
-import { cleanup, fireEvent, render } from '@testing-library/react'; //this is for the DOM testing
-import { screen } from '@testing-library/dom';//this is for the DOM testing
-import { Search } from '../../src/components/Search';
+import {cleanup, fireEvent, render} from '@testing-library/react'; //this is for the DOM testing
+import {screen} from '@testing-library/dom';//this is for the DOM testing
+import Search from '../../src/components/Search';
+import {Link} from "react-router-dom";
+import renderer from 'react-test-renderer';
 
 //TODO: write a test that checks that the search field has some text typed in
 
@@ -12,21 +13,27 @@ import { Search } from '../../src/components/Search';
 
 //Start here: https://testing-library.com/docs/example-findByText
 //Look for the line that says: 'describe('findByText Examples', () => {'
-
-test('REPLACE_ME', async () => {
-    expect("REPLACE_ME").toBe("REPLACE_ME");
+//react-dom check docs
+test('test that checks that the search field has some text', async () => {
+    render (<Search/>)
+    const searchBar = screen.getByDisplayValue('');
+    expect(searchBar.value).toBe("");
+    // Do something
+    fireEvent.change(searchBar, {target: {value: 'Spain'}})
+    expect(searchBar.value).toBe("Spain");
 });
 
 //TODO: write a snapshot test that captures the Search.js component
-
 //Look at this doc to help you write the test: https://jestjs.io/docs/snapshot-testing
 
 test('Search box renders correctly', () => {
-    expect("REPLACE_ME").toBe("REPLACE_ME");
+    const tree = renderer
+        .create(<Search />)
+        .toJSON()
+    expect(tree).toMatchSnapshot()
 });
 
-
-//TODO: write a test to check that the Error component appears if no data 
+//TODO: write a test to check that the Error component appears if no data
 
 //is found from the call to the API. You will need another mock.
 
